2e207ca5bd47917ce3de161149be5b182946adcc
José Monar <jl_mj_17@hotmail.com>
2021-06-22 02:35:57 -0500

    Implementación de dark mode al sitio completo usando javascript.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     eventListeners();
+
+    toggleDarkMode();
 });
 
 function eventListeners() {
@@ -12,4 +14,28 @@ function navegacionResponsiva() {
     const navegacion = document.querySelector(".navegacion");
 
     navegacion.classList.toggle("mostrar");
+}
+
+function toggleDarkMode() {
+    const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+
+    prefiereDarkMode.addEventListener("change", function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    });
+
+    const darkModeBoton = document.querySelector(".dark-mode-boton");
+
+    darkModeBoton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
 }
